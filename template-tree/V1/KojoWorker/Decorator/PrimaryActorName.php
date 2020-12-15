71b5e526be846212554a67fa6c1916bed523f812
Hrvoje Jukic <hrcajuka@gmail.com>
2020-12-15 21:25:42 +0100

    Added custom fabrication files for Decorators.

---
+++ template-tree/V1/KojoWorker/Decorator/PrimaryActorName.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Neighborhoods\BuphaloTemplateTree;
+
+use Neighborhoods\KojoWorkerDecoratorComponent\Worker\AwareTrait;
+use Neighborhoods\KojoWorkerDecoratorComponent\WorkerInterface;
+
+final class PrimaryActorName implements PrimaryActorNameInterface
+{
+    use AwareTrait;
+
+    public function work(): WorkerInterface
+    {
+        /** @neighborhoods-buphalo:annotation-processor Neighborhoods\BuphaloTemplateTree\KojoWorker\Decorator\PrimaryActorName.work
+        // TODO: Implement work() method.
+        throw new \LogicException('Unimplemented custom worker decorator.');
+         */
+        $this->getWorker()->work();
+
+        return $this;
+    }
+}
---
+++ template-tree/V1/KojoWorker/Decorator/PrimaryActorName/Factory.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Neighborhoods\BuphaloTemplateTree\PrimaryActorName;
+
+use Neighborhoods\KojoWorkerDecoratorComponent\Worker\Decorator\Factory as DecoratorFactory;
+
+final class Factory extends DecoratorFactory
+{
+}
