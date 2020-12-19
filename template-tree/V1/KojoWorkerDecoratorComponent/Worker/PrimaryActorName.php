3cb913e683d54a6b17423b8054ab86df96c2a70f
Hrvoje Jukic <hrcajuka@gmail.com>
2020-12-19 18:41:03 +0100

    Added templates for worker and family.

---
+++ template-tree/V1/KojoWorkerDecoratorComponent/Worker/PrimaryActorName.php
@@ -0,0 +1,23 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Neighborhoods\BuphaloTemplateTree;
+
+use Neighborhoods\Kojo\Api\V1;
+
+class PrimaryActorName implements PrimaryActorNameInterface
+{
+    use V1\Worker\Service\AwareTrait;
+    use V1\RDBMS\Connection\Service\AwareTrait;
+
+    public function work(): \Neighborhoods\KojoWorkerDecoratorComponent\WorkerInterface
+    {
+        /** @neighborhoods-buphalo:annotation-processor Neighborhoods\BuphaloTemplateTree\KojoWorker\PrimaryActorName.work
+        // TODO: Implement work() method.
+        throw new \LogicException('Unimplemented custom worker.');
+         */
+
+        return $this;
+    }
+}
---
+++ template-tree/V1/KojoWorkerDecoratorComponent/Worker/PrimaryActorName/Proxy.php
@@ -0,0 +1,60 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Neighborhoods\BuphaloTemplateTree\PrimaryActorName;
+
+use Neighborhoods\BuphaloTemplateTree\PrimaryActorName as Worker;
+use Neighborhoods\ContainerBuilder\Builder;
+use Neighborhoods\Kojo\Api;
+use Neighborhoods\KojoWorkerDecoratorComponent\WorkerInterface;
+use Psr\Container\ContainerInterface;
+use RuntimeException;
+
+class Proxy implements ProxyInterface
+{
+    use Api\V1\Worker\Service\AwareTrait;
+    use Api\V1\RDBMS\Connection\Service\AwareTrait;
+
+    public function work(): WorkerInterface
+    {
+        $worker = $this->getContainer()->get(Worker\Builder\FactoryInterface::class)
+            ->create()
+            ->setApiV1RDBMSConnectionService($this->getApiV1RDBMSConnectionService())
+            ->setApiV1WorkerService($this->getApiV1WorkerService())
+            ->build();
+
+        $worker->work();
+
+        return $this;
+    }
+
+    protected function getContainer(): ContainerInterface
+    {
+        $proteanContainerBuilder = new Builder();
+        $proteanContainerBuilder->setCanBuildZendExpressive(false);
+        $proteanContainerBuilder->setContainerName(str_replace('\\', '', Worker::class));
+        /** @neighborhoods-buphalo:annotation-processor Neighborhoods\BuphaloTemplateTree\KojoWorker\PrimaryActorName.work
+        throw new \LogicException('Discoverable directories path filters not updated.');
+         */
+        $proteanContainerBuilder->getDiscoverableDirectories()->addDirectoryPathFilter(/**
+         *
+         *
+         *
+         * Discoverable directories path filters not updated.
+         */
+        );
+        /** @neighborhoods-buphalo:annotation-processor Neighborhoods\BuphaloTemplateTree\KojoWorker\PrimaryActorName.work
+        throw new \LogicException('Discoverable directories path filters not updated.');
+         */
+        $proteanContainerBuilder->getDiscoverableDirectories()->addDirectoryPathFilter('Prefab5');
+
+        $rootDirectory = realpath(dirname(__DIR__, 3));
+        if (false === $rootDirectory) {
+            throw new RuntimeException('Absolute path of the root directory not found.');
+        }
+        $proteanContainerBuilder->getFilesystemProperties()->setRootDirectoryPath($rootDirectory);
+        $proteanContainerBuilder->registerServiceAsPublic(Worker\Builder\FactoryInterface::class);
+        return $proteanContainerBuilder->build();
+    }
+}
