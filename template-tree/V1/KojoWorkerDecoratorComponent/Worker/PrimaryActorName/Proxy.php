2eeb5b6222bcacd1ebece1737efd5315867e455f
Hrvoje Jukic <hrcajuka@gmail.com>
2021-01-12 11:37:05 +0100

    Removed SearchCriteria from source paths in proxy template.

--- template-tree/V1/KojoWorkerDecoratorComponent/Worker/PrimaryActorName/Proxy.php
+++ template-tree/V1/KojoWorkerDecoratorComponent/Worker/PrimaryActorName/Proxy.php
@@ -52,7 +52,6 @@ class Proxy implements ProxyInterface
             ->addSourcePath('fab/Prefab5/Doctrine')
             ->addSourcePath('fab/Prefab5/PDO')
             ->addSourcePath('fab/Prefab5/Opcache')
-            ->addSourcePath('fab/Prefab5/SearchCriteria')
             ->addSourcePath(/**
              * @neighborhoods-buphalo:annotation-processor Neighborhoods\BuphaloTemplateTree\KojoWorker\PrimaryActorName\Proxy.getContainer
              *
